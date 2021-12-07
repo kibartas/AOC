f937644f0f368374309f7140a478f311c19f8dec
Julius Rasimas <julius.rasimas@revelsystems.com>
2021-12-07 10:21:44 +0200

    day 7

---
+++ 07/part2.js
@@ -0,0 +1,27 @@
+const input = require("fs")
+  .readFileSync(require("path").join(__dirname, "input.txt"), "utf8")
+  .trim()
+  .split(",")
+  .map((el) => parseInt(el));
+
+let result = 0;
+
+let max = 0;
+
+const sum = input.reduce((acc, el) => {
+  if (el > max) max = el;
+  return el + acc;
+}, 0);
+
+const average = Math.floor(sum / input.length);
+
+result = 1e8;
+
+for (let i = 0; i < 2 * average; i++) {
+  const fuelUsed = input.reduce((acc, el) => {
+    return acc + (Math.abs(i - el) * (1 + Math.abs(i - el))) / 2;
+  }, 0);
+  if (fuelUsed < result) result = fuelUsed;
+}
+
+console.log({ result });
